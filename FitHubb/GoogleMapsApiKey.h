5925e353c248b0fe9cd448a078d8ddf82ed412a0
Preston <plphan206@yahoo.com>
2015-10-27 15:48:22 -0700

    Apple Maps integration

---
+++ FitHubb/GoogleMapsApiKey.h
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsApiKey.h
+//  FitHubb
+//
+//  Created by Preston Phan on 10/26/15.
+//  Copyright © 2015 Steven Huynh. All rights reserved.
+//
+
+#ifndef GoogleMapsApiKey_h
+#define GoogleMapsApiKey_h
+
+
+#endif /* GoogleMapsApiKey_h */
+
+let kAPIKey = @"AIzaSyAb8BDmytLPfSyIuCQwn2juaG7wpomP6cw"
+
+
